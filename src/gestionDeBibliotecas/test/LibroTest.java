3f5566f33924a9653af7870644fe93cd4a9ea498
Ubidragon <ubidragon@protonmail.com>
2017-01-30 18:32:45 +0100

    Creacion de clases test (gestion bibliiotecas)

---
+++ src/gestionDeBibliotecas/test/LibroTest.java
@@ -0,0 +1,12 @@
+package gestionDeBibliotecas.test;
+
+
+
+public class LibroTest{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/gestionDeBibliotecas/test/PersonaTest.java
@@ -0,0 +1,10 @@
+package gestionDeBibliotecas.test;
+
+public class PersonaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
